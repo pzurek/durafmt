4f8381de8cfda0499ce15e912f04fd5341c952fa
Piotr Zurek <p.zurek@gmail.com>
2014-09-09 09:42:18 +1200

    better tests

--- durafmt_test.go
+++ durafmt_test.go
@@ -5,29 +5,54 @@ import (
 	"time"
 )
 
+var ins = []time.Duration{
+	time.Duration(time.Second * 12),
+	time.Duration(time.Minute*36 + time.Second*45),
+	time.Duration(time.Hour*42 + time.Minute*42 + time.Second*42),
+}
+
 func TestHMS(t *testing.T) {
-	duration := time.Duration(time.Hour*2 + time.Minute*54 + time.Second*45)
-	d := HMS(duration)
-	s := "02:54:45"
-	if d != s {
-		t.Errorf("HMS: %s, want %s", d, s)
+
+	outs := []string{
+		"00:00:12",
+		"00:36:45",
+		"42:42:42",
+	}
+
+	for i, in := range ins {
+		d := HMS(in)
+		if d != outs[i] {
+			t.Errorf("got: %s, expected %s", d, outs[i])
+		}
 	}
 }
 
 func TestLongWords(t *testing.T) {
-	duration := time.Duration(time.Hour*2 + time.Minute*54 + time.Second*45)
-	d := LongWords(duration)
-	s := "2 hours 54 minutes 45 seconds"
-	if d != s {
-		t.Errorf("HMS: %s, want %s", d, s)
+	outs := []string{
+		"12 seconds",
+		"36 minutes 45 seconds",
+		"42 hours 42 minutes 42 seconds",
+	}
+
+	for i, in := range ins {
+		d := LongWords(in)
+		if d != outs[i] {
+			t.Errorf("got: %s, expected %s", d, outs[i])
+		}
 	}
 }
 
 func TestShortWords(t *testing.T) {
-	duration := time.Duration(time.Hour*2 + time.Minute*54 + time.Second*45)
-	d := ShortWords(duration)
-	s := "2h 54m 45s"
-	if d != s {
-		t.Errorf("HMS: %s, want %s", d, s)
+	outs := []string{
+		"12s",
+		"36m 45s",
+		"42h 42m 42s",
+	}
+
+	for i, in := range ins {
+		d := ShortWords(in)
+		if d != outs[i] {
+			t.Errorf("got: %s, expected %s", d, outs[i])
+		}
 	}
 }
